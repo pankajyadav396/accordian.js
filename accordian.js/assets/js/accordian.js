071b2bd7f0063538052274bc4d804d2442401f6f
Pankaj Yadav-396 <139863536+pankajyaduvanshi-396@users.noreply.github.com>
2023-08-31 11:45:45 +0530

    Add files via upload

---
+++ accordian.js/assets/js/accordian.js
@@ -0,0 +1,11 @@
+let accordian = document.querySelectorAll(".accordian-item");
+
+accordian.forEach((row) => {
+    row.addEventListener("click", () => {
+        const x = document.querySelector(".active");
+
+        row.classList.toggle("active");
+        x && x.classList.remove("active");
+    })
+
+})
